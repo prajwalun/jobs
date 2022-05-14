d5c9435fbe50cc4b5e7c45a165c5df438c6b016a
Prajwal <apple@prajwals-mbp.lan>
2022-05-15 02:25:17 +0530

    ready to deploy

---
+++ src/components/Attribution.js
@@ -0,0 +1,10 @@
+import { Fragment } from "react";
+
+export default function Attribution() {
+    return(
+      <Fragment>
+        <div className="attribution">
+        </div>
+      </Fragment>
+    );
+}
